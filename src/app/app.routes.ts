04ff8e481bc862386e6895739b228276870460bb
unknown <jdvargasr@uniboyaca.edu.co>
2024-09-28 20:18:48 -0500

    reestructuracion del proyecto debido a una falla de persistencia de datos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,18 +8,12 @@ export const routes: Routes = [
     path: '',
     redirectTo:'main',
     pathMatch: 'full'
-},  {
+},
+  {
     path: 'main',
     loadComponent: () => import('./main/main.page').then( m => m.MainPage)
   },
-  {
-    path: 'registro-materia',
-    loadComponent: () => import('./registro-materia/registro-materia.page').then( m => m.RegistroMateriaPage)
-  },
-  {
-    path: 'listado-materias',
-    loadComponent: () => import('./listado-materias/listado-materias.page').then( m => m.ListadoMateriasPage)
-  },
+
 
 
 
--- src/app/models/nota.ts
+++
@@ -1,7 +0,0 @@
-export interface Nota {
-  descripcion: string;
-  nota: number;
-  observaciones: string;
-  fechaEntrega: string;
-  materia:string;
-}
